49bd92d28e60ebf95346f7e59e5f32add1aa42f7
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-02 13:59:16 +0400

    Create User.php

---
+++ src/Entity/User.php
@@ -0,0 +1,33 @@
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    // Getters and setters...
+}
